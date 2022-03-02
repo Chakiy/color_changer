2b908be492645ee137d3883a34039c3b96d78c7b
Chakiy <casilias35@hotmail.com>
2022-03-02 22:13:20 +0100

    range lable added + styled + move bij range +- / output of results addded / clear reset btn updated / progress image added

--- js/index.js
+++ js/index.js
@@ -4,50 +4,76 @@ let colorPrime = document.querySelector("#col1")
 let colorSec = document.querySelector("#col2")
 
 
+let degLabel = document.querySelector("#deg_info")
 
 // const col = document.querySelector("#col")
 
 const clear = document.querySelector("#clear")
 const container = document.querySelector(".container")
 
+
+let output = document.querySelector(".output")
+
 // conslole log
 
 console.log(deg)
-console.log(colorPrime)
+console.log(colorPrime.value)
 console.log(colorSec)
 
 
 console.log(container)
 
 
+
+
+// colorPrime.value = "#adffff"
+
+
 deg.addEventListener('input', function(e) {
     console.log(deg.value)
-    container.style.background = `linear-gradient(${deg.value}deg, ${colorPrime}, ${colorSec} )`;
+    degLabel.textContent = deg.value
+
     
+    let bulletPosition = (deg.value / deg.max);
+    degLabel.style.left = (bulletPosition * 180) + "px";
+
+    container.style.background = `linear-gradient(${deg.value}deg, ${colorPrime.value}, ${colorSec.value} )`;
+    output.textContent = `background: linear-gradient(${deg.value}deg, ${colorPrime.value}, ${colorSec.value} )`;
 }); 
 
 
 colorPrime.addEventListener('input', function(e) {
     
-   colorPrime = e.target.value
-    container.style.background = `linear-gradient(${deg.value}deg, ${colorPrime.value}, ${colorSec} )`;
+   colorPrime.value = e.target.value
+    container.style.background = `linear-gradient(${deg.value}deg, ${colorPrime.value}, ${colorSec.value} )`;
+    output.textContent = `background: linear-gradient(${deg.value}deg, ${colorPrime.value}, ${colorSec.value} )`;
+
 }); 
 
 
 
 
 colorSec.addEventListener('input', function(e) {
-   colorSec = e.target.value
-   container.style.background = `linear-gradient(${deg.value}deg, ${colorPrime}, ${colorSec} )`;
+   colorSec.value = e.target.value
+   container.style.background = `linear-gradient(${deg.value}deg, ${colorPrime.value}, ${colorSec.value} )`;
+   output.textContent = `background: linear-gradient(${deg.value}deg, ${colorPrime.value}, ${colorSec.value} )`;
+
 }); 
 
 
-clear.addEventListener("click", function(){
+clear.addEventListener("click", function(e){
+    e.preventDefault()
     container.style.background = "#ffffff"
    
-    deg.value = ""
-    colorPrime.value = ""
-    colorSec.value = ""
+    deg.value = "0"
+    
+    colorPrime.value = "#f8a5f8"
+    
+    colorSec.value = "#adffff"
+    output.textContent = "Background..."
+    degLabel.style.left = 0
+    degLabel.textContent = "0"
+
 })
 
 
