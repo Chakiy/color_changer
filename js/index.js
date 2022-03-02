3e43916239d9efb4ee6128f238d338308faaa7eb
Chakiy <casilias35@hotmail.com>
2022-03-02 19:57:41 +0100

    input type  color pallet added / input range type added / css styled

--- js/index.js
+++ js/index.js
@@ -1,9 +1,12 @@
 
-const deg = document.querySelector("#deg")
-const colorPrime = document.querySelector("#col1")
-const colorSec = document.querySelector("#col2")
+let deg = document.querySelector("#deg")
+let colorPrime = document.querySelector("#col1")
+let colorSec = document.querySelector("#col2")
 
-const bg = document.querySelector("#bg")
+
+
+// const col = document.querySelector("#col")
+
 const clear = document.querySelector("#clear")
 const container = document.querySelector(".container")
 
@@ -13,48 +16,39 @@ console.log(deg)
 console.log(colorPrime)
 console.log(colorSec)
 
-console.log(bg)
+
 console.log(container)
 
 
-let degChange = 0
 deg.addEventListener('input', function(e) {
-    // console.log(e.target.value)
-    degChange = e.target.value.trim().substring(0,3)
-    container.style.background = `linear-gradient(${degChange}deg, #${colPrChange}, #${colSecChange} )`;
-    // console.log(degChange)
+    console.log(deg.value)
+    container.style.background = `linear-gradient(${deg.value}deg, ${colorPrime}, ${colorSec} )`;
+    
 }); 
 
-let colPrChange = "0099F7"
 
 colorPrime.addEventListener('input', function(e) {
-    console.log(e.target.value)
-    colPrChange = e.target.value.trim().substring(0,6)
-    container.style.background = `linear-gradient(${degChange}deg, #${colPrChange}, #${colSecChange} )`;
-    console.log(colPrChange)
+    
+   colorPrime = e.target.value
+    container.style.background = `linear-gradient(${deg.value}deg, ${colorPrime.value}, ${colorSec} )`;
 }); 
 
 
-let colSecChange = "F11712"
+
 
 colorSec.addEventListener('input', function(e) {
-    console.log(e.target.value)
-    colSecChange = e.target.value.trim().substring(0,6)
-    container.style.background = `linear-gradient(${degChange}deg, #${colPrChange}, #${colSecChange} )`;
-    console.log(colSecChange)
+   colorSec = e.target.value
+   container.style.background = `linear-gradient(${deg.value}deg, ${colorPrime}, ${colorSec} )`;
 }); 
 
-const colors = [["#0099F7", "#F11712"], ["#0caaF7", "#F1aaa2"],["#5A3F37", "#2C7744"], ["#4DA0B0", "#D39D38" ]]
-
-bg.addEventListener("click", function(){
-
-    container.style.background = `linear-gradient(${degChange}deg, #${colPrChange}, #${colSecChange} )`;
-})
 
 clear.addEventListener("click", function(){
     container.style.background = "#ffffff"
-    console.log(deg)
+   
     deg.value = ""
     colorPrime.value = ""
     colorSec.value = ""
 })
+
+
+
